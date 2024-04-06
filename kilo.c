ac5dc7845be93bfd54f5396ef724ae2a33fe7d3f
krishmaha <krish@mahas.net>
2024-04-07 00:38:22 +0100

    Enter into raw mode

--- kilo.c
+++ kilo.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 struct termios origTermios;
 
@@ -18,8 +19,16 @@ void enableRawMode() {
   struct termios raw = origTermios;
 
   // turns off echo and canonical so we read input byte by byte rather 
-  // than line by line
-  raw.c_lflag &= ~(ECHO | ICANON);
+  // than line by line. Also allows keypresses that would otherwise terminate program
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  // set character size to 8 bits ber byte
+  raw.c_cflag |= (CS8);
+  // minimum number of input bytes needed before read() can return 
+  raw.c_cc[VMIN] = 0;
+  // maximum time to wait (in tenths of a second) before read() can return
+  raw.c_cc[VTIME] = 1;
 
   // applies new flags to the terminal, after all pending output has been 
   // written and discarding unread input
@@ -29,9 +38,17 @@ void enableRawMode() {
 
 int main() {
   enableRawMode();
-  char c;
-  // chars after q will be left unread on the input queue
-  // fed into shell after program exits
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+  while (1) {
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') {
+      break;
+    }
+  }
   return 0;
 }
