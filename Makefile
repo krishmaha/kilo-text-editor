a61b6c5bc05b803709c0413b242e282135737e88
krishmaha <krish@mahas.net>
2024-04-09 21:04:52 +0100

    Update Makefile and refactor keyboard input

--- Makefile
+++ Makefile
@@ -1,6 +1,37 @@
-# kilo is what we want to build, kilo.c is what is required to build it
-kilo: kilo.c
-	$(CC) kilo.c -o kilo -Wall -Wextra -pedantic -std=c99
+# # kilo is what we want to build, kilo.c is what is required to build it
+# kilo: kilo.c
+# 	$(CC) kilo.c -o kilo -Wall -Wextra -pedantic -std=c99
 
-run: kilo
-	./kilo
+# run: kilo
+# 	./kilo
+
+CC = gcc
+CFLAGS = -Iinclude -Wall -Wextra -pedantic
+LDFLAGS = 
+SRCDIR = src
+OBJDIR = obj
+BINDIR = bin
+# generate list of all .c files in src directory
+SOURCES = $(wildcard $(SRCDIR)/*.c)
+# create list of object file paths from list of source file paths
+OBJECTS = $(SOURCES:$(SRCDIR)/%.c=$(OBJDIR)/%.o)
+TARGET = $(BINDIR)/kilo
+
+# rule for building target from prerequisite (object files). After colon: prerequisite
+# $^: list of prerequisites (object files), $@: name of the target
+$(TARGET): $(OBJECTS)
+	mkdir -p $(BINDIR)
+	$(CC) $(LDFLAGS) $^ -o $@
+
+# rule to create object files from .c files. After colon: prerequisite
+# -c: compile source file into object file but don't link it. Generates .o file
+# $<: represents the first prerequisite of the rule
+$(OBJDIR)/%.o: $(SRCDIR)/%.c
+	mkdir -p $(OBJDIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+run: 
+	$(BINDIR)/kilo
+
+clean: 
+	rm -rf $(OBJDIR) $(BINDIR)
--- src/main.c
+++ src/main.c
@@ -4,27 +4,54 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <errno.h>
-
 #include "terminal.h"
+
+// converts letter to control key equivalent by zeroing all but lowest 5 bits
+#define CTRL_KEY(k) ((k) & 0x1f)
 
 struct termios origTermios;
 
+char editorReadKey() {
+  int nread;
+  char c;
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+    if (nread == -1 && errno != EAGAIN) {
+      die("read");
+    } 
+  }
+  return c;
+}
+
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+      exit(0);
+      break;
+  }
+}
+
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+}
 
 int main() {
   enableRawMode();
   while (1) {
-    char c = '\0';
-    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-      die("read");
-    }
-    if (iscntrl(c)) {
-      printf("%d\r\n", c);
-    } else {
-      printf("%d ('%c')\r\n", c, c);
-    }
-    if (c == 'q') {
-      break;
-    }
+    editorRefreshScreen();
+    editorProcessKeypress();
+    // char c = '\0';
+    // if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+    //   die("read");
+    // }
+    // if (iscntrl(c)) {
+    //   printf("%d\r\n", c);
+    // } else {
+    //   printf("%d ('%c')\r\n", c, c);
+    // }
+    // if (c == ('q')) {
+    //   break;
+    // }
   }
   return 0;
 }
